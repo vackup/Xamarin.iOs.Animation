30140158e3d8fe745aa23d7dcc88b0d4d80d9fa7
Hernan Zaldivar <hzaldivar79@gmail.com>
2016-10-03 21:31:35 -0300

    Initial checkin with the animation

---
+++ Animation/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace Animation
+{
+    public static class Extensions
+    {
+        public static void Rotate(this UIView view, bool isIn, bool fromLeft = true, double duration = 0.3, Action onFinished = null)
+        {
+            var minTransform = CGAffineTransform.MakeRotation((nfloat)((fromLeft ? -1 : 1) * DegreeToRadian(45)));
+            var maxTransform = CGAffineTransform.MakeRotation((nfloat)0.0);
+            
+            UIView.Animate(duration, 0, UIViewAnimationOptions.CurveEaseInOut,
+                () => {
+                    view.Transform = isIn ? maxTransform : minTransform;
+                },
+                onFinished
+            );
+        }
+
+        private static double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+    }
+}
---
+++ Animation/ViewController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace Animation
+{
+    public partial class ViewController : UIViewController
+    {
+        private bool _isIn = true;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var image = UIImage.FromBundle("add_btn");
+
+            var shareButton = UIButton.FromType(UIButtonType.Custom);
+            shareButton.SetImage(image, UIControlState.Normal);
+            shareButton.SetImage(image, UIControlState.Highlighted);
+
+            shareButton.Frame = new CGRect(
+                this.View.Frame.Width / 2,
+                this.View.Frame.Height / 2,
+                image.Size.Width,
+                image.Size.Height);
+
+            shareButton.TouchUpInside += (sender, e) =>
+            {
+                var button = sender as UIButton;
+
+                button.Rotate(_isIn, false);
+                _isIn = !_isIn;
+            };
+
+            View.Add(shareButton);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
